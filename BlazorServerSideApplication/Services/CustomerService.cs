8007ab889d0c63a299eb9c72eea92b3a414fc852
trenin.georgii <georgii.trenin@devexpress.com>
2020-08-18 16:15:59 +0300

    code with error

---
+++ BlazorServerSideApplication/Services/CustomerService.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Xpo;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSideApplication.Services {
+
+    public abstract class BaseService {
+        readonly IDataLayer dataLayer;
+        protected readonly UnitOfWork readUnitOfWork;
+        public BaseService(IDataLayer dataLayer, UnitOfWork modificationUnitOfWork) {
+            this.dataLayer = dataLayer;
+            readUnitOfWork = modificationUnitOfWork;
+        }
+        protected UnitOfWork CreateModificationUnitOfWork() {
+            return new UnitOfWork(dataLayer);
+        }
+    }
+
+    public class CustomerService : BaseService {
+        public CustomerService(IDataLayer dataLayer, UnitOfWork modificationUnitOfWork)
+            : base(dataLayer, modificationUnitOfWork) { }
+        public Task<IQueryable<Customer>> Get() {
+            var query = (IQueryable<Customer>)readUnitOfWork.Query<Customer>();
+            return Task.FromResult(query);
+        }
+        public async Task<Customer> Add(Dictionary<string, object> values) {
+            string json = JsonConvert.SerializeObject(values);
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var newCustomer = JsonPopulateObjectHelper.PopulateObject<Customer>(json, uow);
+                await uow.CommitChangesAsync();
+                return await readUnitOfWork.GetObjectByKeyAsync<Customer>(newCustomer.Oid, true);
+            }
+        }
+        public async Task<Customer> Update(int oid, Dictionary<string, object> values) {
+            string json = JsonConvert.SerializeObject(values);
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var customer = await uow.GetObjectByKeyAsync<Customer>(oid);
+                JsonPopulateObjectHelper.PopulateObject(json, uow, customer);
+                await uow.CommitChangesAsync();
+            }
+            return await readUnitOfWork.GetObjectByKeyAsync<Customer>(oid, true);
+        }
+        public async Task Delete(int oid) {
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var customer = await uow.GetObjectByKeyAsync<Customer>(oid);
+                customer.Delete();
+                await uow.CommitChangesAsync();
+            }
+        }
+    }
+}
---
+++ BlazorServerSideApplication/Services/OrderService.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Xpo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServerSideApplication.Services {
+    public class OrderService : BaseService {
+        public OrderService(IDataLayer dataLayer, UnitOfWork readUnitOfWork)
+            : base(dataLayer, readUnitOfWork) { }
+        public Task<IQueryable<Order>> Get() {
+            var query = (IQueryable<Order>)readUnitOfWork.Query<Order>();
+            return Task.FromResult(query);
+        }
+        public Task<IQueryable<Order>> GetCustomerOrders(int customerOid) {
+            var query = readUnitOfWork.Query<Order>().Where(order => order.Customer.Oid == customerOid);
+            return Task.FromResult(query);
+        }
+        public async Task<Order> Add(Dictionary<string, object> values, int customerOid) {
+            string json = JsonConvert.SerializeObject(values);
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var customer = await uow.GetObjectByKeyAsync<Customer>(customerOid);
+                var newOrder = JsonPopulateObjectHelper.PopulateObject<Order>(json, uow);
+                newOrder.Customer = customer;
+                await uow.CommitChangesAsync();
+                return await readUnitOfWork.GetObjectByKeyAsync<Order>(newOrder.Oid, true);
+            }
+        }
+        public async Task<Order> Update(int oid, Dictionary<string, object> values) {
+            string json = JsonConvert.SerializeObject(values);
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var order = await uow.GetObjectByKeyAsync<Order>(oid);
+                JsonPopulateObjectHelper.PopulateObject(json, uow, order);
+                await uow.CommitChangesAsync();
+            }
+            return await readUnitOfWork.GetObjectByKeyAsync<Order>(oid, true);
+        }
+        public async Task Delete(int oid) {
+            using(UnitOfWork uow = CreateModificationUnitOfWork()) {
+                var order = await uow.GetObjectByKeyAsync<Order>(oid);
+                order.Delete();
+                await uow.CommitChangesAsync();
+            }
+        }
+    }
+}
